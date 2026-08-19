96d87eef6c5c2ab9d23ac8a33727dc793bd8cd04
agungshndy <agungshandy43@gmail.com>
2026-08-19 19:38:21 +0800

    add logical expression for habitcard

--- src/components/HabitCard.jsx
+++ src/components/HabitCard.jsx
@@ -3,16 +3,21 @@ import { useState } from "react"
 function HabitCard(){
 
     const [ streak,setStreak ] = useState(0);
+    const [ button, setButton ] = useState("Check In");
+
+    const toggleButton = () => {
+        setButton("Done Today");
+    }
 
     return (
         <div className="habitcard">
             <div className="section-1 grid-cols-2 text-sm border rounded-md mx-10">
                 <div className="sectionbar-1 flex grid-cols-2 justify-around">
-                    <div className="flex mx-2 gap-3">
+                    <div className="flex mx-2 my-2 gap-3">
                         <span>Morning run</span>
                         <p>{streak} day streak</p>
                     </div>
-                    <button className="border rounded-md px-2 py-1">Done Today</button>
+                    <button className="border rounded-md px-2 py-1 my-2 hover:cursor-pointer" onClick={toggleButton}>{button}</button>
                 </div>
                 <div className="sectionbar-2">
                     <div className="grid grid-cols-28 gap-1">
@@ -49,13 +54,11 @@ function HabitCard(){
             </div>
             <div className="section-1 grid-cols-2 text-sm border rounded-md mx-10 my-5">
                 <div className="sectionbar-1 flex grid-cols-3 justify-evenly">
-                    <div className="mx-2">
-                        <p>Read 20 pages</p>
-                    </div>
-                    <div className="mx-2">
+                    <div className="flex mx-2 my-2 gap-3">
+                        <span>Read 20 pages</span>
                         <p>{streak} day streak</p>
                     </div>
-                    <button className="border rounded-md px-2 py-1">Check In</button>
+                    <button className="border rounded-md px-2 py-1 my-2 hover:cursor-pointer" onClick={toggleButton}>{button}</button>
                 </div>
                 <div className="sectionbar-2">
                     <div className="grid grid-cols-28 gap-1">
